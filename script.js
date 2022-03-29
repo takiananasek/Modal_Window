bb1d176490f600c9da09751016c1d191c1f14cfc
Nina Marszałek <76755039+takiananasek@users.noreply.github.com>
2022-03-29 15:59:20 +0200

    script commit

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', function () {
+    console.log('Button clicked');
+    openModal();
+  });
+}
+
+btnCloseModal.addEventListener('click', function () {
+  closeModal();
+});
+
+overlay.addEventListener('click', function () {
+  closeModal();
+});
+
+const closeModal = function () {
+  overlay.classList.add('hidden');
+  modal.classList.add('hidden');
+};
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    if (!modal.classList.contains('hidden')) {
+      closeModal();
+    }
+  }
+});
